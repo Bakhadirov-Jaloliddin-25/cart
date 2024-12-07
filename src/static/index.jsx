6e54c075f04aa0c0e39b6b4e6f3ac10d7bd70cfa
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2024-12-08 02:18:38 +0500

    first commit

---
+++ src/static/index.jsx
@@ -0,0 +1,22 @@
+export const LINKS = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Discovery",
+    path: "/discovery",
+  },
+  {
+    id: 3,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 4,
+    name: "Contact us",
+    path: "/contact",
+  },
+];
